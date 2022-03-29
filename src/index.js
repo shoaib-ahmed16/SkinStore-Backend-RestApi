8aecc1cd376c138132d3b0dc4c2e0425d048d59f
Bvaishnavi123 <97448974+Bvaishnavi123@users.noreply.github.com>
2022-03-29 17:00:02 +0530

    -

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ const app=express();
 app.use(express.json())
 
 const {register,login,generateToken}=require("./controllers/register&login.controller")
-
+const productController = require("./controllers/ProductController")
 
 
 app.post("/register",register)
 
 app.post("/login",login)
+
+
+
 
 
 
