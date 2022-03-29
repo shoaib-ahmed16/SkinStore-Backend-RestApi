4471548a6281e29427d9e3b3faeb5235d553e4f7
Bvaishnavi123 <97448974+Bvaishnavi123@users.noreply.github.com>
2022-03-29 17:33:06 +0530

    Product Schema and controller

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ app.use(express.json())
 const {register,login,generateToken}=require("./controllers/register&login.controller")
 const productController = require("./controllers/ProductController")
 
+app.use("/product1",productController)
+app.use("/product2",productController)
+app.use("/product3",productController)
+
 
 app.post("/register",register)
 
