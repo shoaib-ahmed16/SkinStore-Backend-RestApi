02c15cb53ba898bd53da1a1814bb43fb66e177d5
Bvaishnavi123 <97448974+Bvaishnavi123@users.noreply.github.com>
2022-03-29 20:04:58 +0530

    Product Schema and controller

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Product = require("../Models/ProductModel");
+const Product = require("../models/ProductModel");
 const Uploads = require("../Middlewares/Uploads")
 
 router.get("/", async (req, res) => {
