c05d20cda4da546f1f0dfcbc22e9ee4e73ceb6e6
Bvaishnavi123 <97448974+Bvaishnavi123@users.noreply.github.com>
2022-03-30 11:10:07 +0530

    id added

--- src/models/ProductModel1.js
+++ src/models/ProductModel1.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 
 
 const ProductSchema=mongoose.Schema({
+    id:{type:String,required:true,unique:true},
     productPic :{type:String,required:true},
     productName:{type:String,required:true},
     productBrand : {type:String,required:true},
