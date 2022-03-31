5cf6bcc9264e37bc35fef2a407b6b5c559201b16
Shoaib Ahmed <96101435+shoaib-ahmed16@users.noreply.github.com>
2022-03-31 18:26:06 +0530

    ada

--- src/index.js
+++ src/index.js
@@ -1,55 +1,58 @@
-const express =require("express");
-const cors = require("cors")
+const express = require('express')
+const cors = require('cors')
 
-const userschema =require("./controllers/dummy.user.controller.js")
+const userschema = require('./controllers/dummy.user.controller.js')
 
-const cartController =require("./controllers/cart.controller.js")
+const cartController = require('./controllers/cart.controller.js')
 
-const productController1= require("./controllers/ProductController1.js")
+const productController1 = require('./controllers/ProductController1.js')
 
-const productController2 = require("./controllers/ProductController2.js")
+const productController2 = require('./controllers/ProductController2.js')
 
-const productController3 = require("./controllers/ProductController3.js")
+const productController3 = require('./controllers/ProductController3.js')
 
+const addressController = require('./controllers/address.controller.js')
 
-const addressController =require("./controllers/address.controller.js")
+const {
+  register,
+  login,
+  generateToken,
+} = require('./controllers/registerlogin.controller')
 
-const {register,login,generateToken}=require("./controllers/registerlogin.controller")
+const passport = require('./configs/google-oauth')
 
-const passport=require("./configs/google-oauth")
-
-const app = express();
+const app = express()
 
 app.use(cors())
 
 app.use(express.json())
 
+app.use('/user', userschema) //extra use less api use to remove error
+app.use('/cartproduct', cartController)
+app.use('/product1', productController1)
+app.use('/product2', productController2)
+app.use('/product3', productController3)
+app.use('/address', addressController)
 
-app.use("/user",userschema)  //extra use less api use to remove error
-app.use("/cartproduct",cartController)
-app.use("/product1",productController1)
-app.use("/product2",productController2)
-app.use("/product3",productController3)
-app.use("/address",addressController)
+app.post('/register', register)
+app.post('/login', login)
 
+app.get(
+  '/auth/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] })
+)
 
-
-app.post("/register",register)
-app.post("/login",login)
-
-
-
-
-app.get('/auth/google',
-  passport.authenticate('google', { scope: ['profile','email'] }));
- 
-app.get('/auth/google/callback', 
-  passport.authenticate('google', { failureRedirect: '/login',session:false }),
-  function(req, res) {
+app.get(
+  '/auth/google/callback',
+  passport.authenticate('google', {
+    failureRedirect: '/login',
+    session: false,
+  }),
+  function (req, res) {
     // Successful authentication, redirect home.
-    res.redirect('https://www.skinstore.com');
+    res.redirect('https://www.skinstore.com')
     console.log(generateToken())
-  });
+  }
+)
 
-
-module.exports=app;
+module.exports = app
