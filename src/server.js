c9307c819179121fd85a9beafec9d343cce05dd5
Shoaib Ahmed <96101435+shoaib-ahmed16@users.noreply.github.com>
2022-03-31 11:34:34 +0530

    port changes

--- src/server.js
+++ src/server.js
@@ -1,16 +1,13 @@
-const connect =require("./configs/database.js")
+const connect = require('./configs/database.js')
 
-const app =require("./index.js")
+const app = require('./index.js')
 
+app.listen(4969, async (req, res) => {
+  try {
+    await connect()
 
-app.listen(5000,async(req,res)=>{
-    try{
-        await connect()
-
-        console.log("Listening to localhost 5000")
-    }
-    catch (err)
-    {
-        console.log({message:err.message})
-    }
+    console.log('Listening to localhost 5000')
+  } catch (err) {
+    console.log({ message: err.message })
+  }
 })
